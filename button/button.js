8742fde7e026ddfeff78433216d9d950c8e3f8ec
maxwell3025 <44588341+maxwell3025@users.noreply.github.com>
2022-02-14 20:17:07 -0500

    created button that changes number
    I am now able to store data on the website

---
+++ button/button.js
@@ -0,0 +1,43 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+import { getDatabase, ref, runTransaction, onValue } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBTdt5AHj_bWQeyZ4wif_dQB2vrWMQxmgU",
+    authDomain: "website-counter-8d17e.firebaseapp.com",
+    databaseURL: "https://website-counter-8d17e-default-rtdb.firebaseio.com",
+    projectId: "website-counter-8d17e",
+    storageBucket: "website-counter-8d17e.appspot.com",
+    messagingSenderId: "747480479544",
+    appId: "1:747480479544:web:11e686d632b0829d5cf3cd",
+    measurementId: "G-WTQ2EQM18Y"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const database = getDatabase(app);
+
+var counterref = ref(database,'counter');
+
+var counterText = d3.select("#counter");
+
+function increment(){
+    runTransaction(counterref, post=>post+1);
+}
+
+function decrement(){
+    runTransaction(counterref, post=>post-1);
+}
+
+window.onload = () => {
+    d3.select("#increase-button").on("click", ()=>
+    {
+        increment();
+    })
+    d3.select("#decrease-button").on("click", ()=>
+    {
+        decrement();
+    })
+    onValue(counterref, value=>{
+        counterText.text(value.val());
+    });
+}
