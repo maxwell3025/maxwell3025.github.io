adc0e664517b30cebf74764e2af9b4ca546e0691
maxwell3025 <amoinus@gmail.com>
2022-09-02 17:20:00 -0400

    Added Bounce Physics
    -added collisions with walls and other balls
    -still need to add angular stuff
    -need to improve starting conditions

--- home/home.js
+++ home/home.js
@@ -3,60 +3,145 @@ class PhysicsBall{
         this.image = image;
         this.link = link;
         this.title = title;
-        //this.x = Math.random();
-        //this.y = Math.random();
-        //this.a = Math.random() * Math.PI * 2;
-        //this.r = Math.random() * 0.25;
-        this.x = 0.5;
-        this.y = 0.5;
-        this.a = 0;
-        this.r = 0.25;
+        this.x = Math.random();
+        this.y = Math.random();
+        this.a = Math.random() * Math.PI * 2;
+        this.r = Math.random() * 0.125 + 0.125;
+        //this.x = 0.5;
+        //this.y = 0.5;
+        //this.a = 0;
+        //this.r = 0.25;
         this.vx = 0;
         this.vy = 0;
         this.va = 0;
     }
+
+    get pos(){
+        return [this.x, this.y]
+    }
+    
+    get vel(){
+        return [this.vx, this.vy]
+    }
+}
+
+const dot = (a, b) => {
+    return a[0] * b[0] + a[1] * b[1]
+}
+
+const scale = (a, scale) => {
+    return [a[0] * scale, a[1] * scale]
+}
+
+const proj = (a, b) => {
+    return scale(b, dot(a, b) / dot(b, b))
+}
+
+const add = (a, b) => {
+    return [a[0] + b[0], a[1] + b[1]]
+}
+
+const sub = (a, b) => {
+    return [a[0] - b[0], a[1] - b[1]]
+}
+
+const g = 1;
+const stepPhysics = (ballList, dt) => {
+    ballList.forEach(ball => {
+        //direct integration
+            ball.vx += 0;
+            ball.vy += dt * g;
+            ball.x += dt * ball.vx;
+            ball.y += dt * ball.vy;
+            ball.a += dt * ball.va;
+        //wall collisions
+            if(ball.y - ball.r < 0){
+                ball.vy = Math.abs(ball.vy)
+            }
+            if(ball.y + ball.r > 1){
+                ball.vy = -Math.abs(ball.vy)
+            }
+            if(ball.x - ball.r < 0){
+                ball.vx = Math.abs(ball.vx)
+            }
+            if(ball.x + ball.r > 1){
+                ball.vx = -Math.abs(ball.vx)
+            }
+        //other ball collisions
+            ballList.forEach(other => {
+                if(ball != other){
+                    let dx = ball.x - other.x;
+                    let dy = ball.y - other.y;
+                    let rTotal = ball.r * 0.5 + other.r * 0.5;
+                    if(dx * dx + dy * dy <= rTotal * rTotal && dot(sub(ball.vel, other.vel), sub(ball.pos, other.pos)) <= 0){
+                        //head-on collision math
+                            let velocityProjA = proj(ball.vel, [dx, dy]);
+                            let velocityProjB = proj(other.vel, [dx, dy]);
+                            let ma = ball.r * ball.r;
+                            let mb = other.r * other.r;
+                            let changeVA = scale(sub(velocityProjB, velocityProjA), 2 * mb / (ma + mb))
+                            let changeVB = scale(sub(velocityProjA, velocityProjB), 2 * ma / (ma + mb))
+                        //update velocities
+                            ball.vx += changeVA[0]
+                            ball.vy += changeVA[1]
+                            other.vx += changeVB[0]
+                            other.vy += changeVB[1]
+                    }
+                }
+            })
+    });
 }
 
 window.onload = () => {
     let projectMenu = d3.select("#project_menu");
     let ballList = [];
 
-    ballList.push(new PhysicsBall("/resources/thumbnail-gravity-sim.png", "../gravity", "Gravity Simulation"));
+    ballList.push(new PhysicsBall("/resources/thumbnail-gravity-sim.png", "../gravity", "Gravity Simulation"))
+    ballList.push(new PhysicsBall("/resources/nerd_face.png", "", ""))
 
     let renderedBalls = projectMenu.selectAll(".ball")
-    .data(ballList)
-    .enter()
-    .append("g")
-    .attr("transform", function(data){return `translate(${data.x * 100}, ${data.y * 100}) scale(${data.r}, ${data.r}) rotate(${data.a * 180 / Math.PI}, 0, 0)`});
+        .data(ballList)
+        .enter()
+        .append("g")
+        .attr("transform", function(data){return `translate(${data.x * 100}, ${data.y * 100}) scale(${data.r}, ${data.r}) rotate(${data.a * 180 / Math.PI}, 0, 0)`});
 
     renderedBalls.each(function (data, index) {
         let currentBall = d3.select(this)
-        .classed("thumb", true)
+            .classed("thumb", true)
 
         .append("a")
-        .attr("href", data.link);
+            .attr("href", data.link);
 
         currentBall.append("image")
-        .attr("x", -50)
-        .attr("y", -50)
-        .attr("width", 100)
-        .attr("height", 100)
-        .attr("href", data.image)
-        .attr("clip-path", "url(#circle-clip)")
+            .attr("x", -50)
+            .attr("y", -50)
+            .attr("width", 100)
+            .attr("height", 100)
+            .attr("href", data.image)
+            .attr("clip-path", "url(#circle-clip)")
 
-        currentBall.append("rect")
-        .attr("x",-50)
-        .attr("y",-4)
-        .attr("width",100)
-        .attr("height",8)
-        .attr("clip-path", "url(#circle-clip)")
-        .classed("appearOnHover", true)
+        if(data.title != ""){
+            currentBall.append("rect")
+                .attr("x",-50)
+                .attr("y",-4)
+                .attr("width",100)
+                .attr("height",8)
+                .attr("clip-path", "url(#circle-clip)")
+                .classed("appearOnHover", true)
 
-        currentBall.append("text")
-        .attr("x",0)
-        .attr("y",0)
-        .classed("appearOnHover", true)
-        .classed("label", true)
-        .text(data.title)
+            currentBall.append("text")
+                .attr("x",0)
+                .attr("y",0)
+                .classed("appearOnHover", true)
+                .classed("label", true)
+                .text(data.title)
+        }
     });
+
+    setInterval(() => {
+        renderedBalls
+            .attr("transform", function(data){return `translate(${data.x * 100}, ${data.y * 100}) scale(${data.r}, ${data.r}) rotate(${data.a * 180 / Math.PI}, 0, 0)`});
+
+        stepPhysics(ballList, 0.004);
+    }, 4)
 }
