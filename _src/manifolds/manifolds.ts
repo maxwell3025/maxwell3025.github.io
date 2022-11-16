2a5800b8858780c187e6f31b57a8fb2cb2e96564
maxwell3025 <44588341+maxwell3025@users.noreply.github.com>
2022-11-16 15:51:28 -0500

    Added Controls For Annealing Process
    -you can now stretch, flatten and anneal your manifold as you like!

--- _src/manifolds/manifolds.ts
+++ _src/manifolds/manifolds.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
 import { Vec3, Vec2 } from './geometry';
 import { GeoMesh } from './topologysolver';
 
@@ -21,78 +22,110 @@ function fromGeoMesh(geoMesh: GeoMesh): THREE.BufferGeometry {
 
 //row-major order
 function metric(coords: Vec2): [number, number, number, number] {
-  return [1, 0, 0, 1];
+  return [1, 0, 0, 4];
 }
 
-const resolution = 16;
-const initialNoise = 0.1;
-const cellWidth = 1.0 / resolution;
+const meshSettings = {
+  minX: -1,
+  maxX: 1,
+  minY: -1,
+  maxY: 1,
+  resolution: 16,
+  initialNoise: 0.1,
+  cellWidth: 1.0 / 16,
+};
 
-let myGeoMesh = new GeoMesh();
+function initialize(): GeoMesh {
+  let outputMesh = new GeoMesh();
 
-for (let y = 0; y <= resolution; y++) {
-  for (let x = 0; x <= resolution; x++) {
-    myGeoMesh.points.push(
-      new Vec3(
-        x * cellWidth,
-        y * cellWidth,
-        initialNoise * (Math.random() - 0.5)
-      )
-    );
+  for (let y = 0; y <= meshSettings.resolution; y++) {
+    for (let x = 0; x <= meshSettings.resolution; x++) {
+      outputMesh.points.push(
+        new Vec3(
+          meshSettings.minX +
+            x *
+              meshSettings.cellWidth *
+              (meshSettings.maxX - meshSettings.minX),
+          meshSettings.minY +
+            y *
+              meshSettings.cellWidth *
+              (meshSettings.maxY - meshSettings.minY),
+          meshSettings.initialNoise * (Math.random() - 0.5)
+        )
+      );
+      if (
+        x == 0 ||
+        y == 0 ||
+        x == meshSettings.resolution ||
+        y == meshSettings.resolution
+      ) {
+        outputMesh.flattenWeights.push(0);
+      } else {
+        outputMesh.flattenWeights.push(1);
+      }
+    }
   }
-}
 
-for (let y = 0; y < resolution; y++) {
-  for (let x = 0; x < resolution; x++) {
-    myGeoMesh.connections.push(
-      new Vec3(x + y * (resolution + 1), 1 + x + y * (resolution + 1), 0),
+  for (let y = 0; y < meshSettings.resolution; y++) {
+    for (let x = 0; x < meshSettings.resolution; x++) {
+      outputMesh.connections.push(
+        new Vec3(
+          x + y * (meshSettings.resolution + 1),
+          1 + x + y * (meshSettings.resolution + 1),
+          0
+        ),
+        new Vec3(
+          x + y * (meshSettings.resolution + 1),
+          meshSettings.resolution + 1 + x + y * (meshSettings.resolution + 1),
+          0
+        ),
+        new Vec3(
+          x + y * (meshSettings.resolution + 1),
+          meshSettings.resolution + 2 + x + y * (meshSettings.resolution + 1),
+          0
+        )
+      );
+    }
+  }
+
+  for (let i = 0; i < meshSettings.resolution; i++) {
+    outputMesh.connections.push(
       new Vec3(
-        x + y * (resolution + 1),
-        resolution + 1 + x + y * (resolution + 1),
+        meshSettings.resolution * (meshSettings.resolution + 1) + i,
+        meshSettings.resolution * (meshSettings.resolution + 1) + i + 1,
         0
       ),
       new Vec3(
-        x + y * (resolution + 1),
-        resolution + 2 + x + y * (resolution + 1),
+        i * (meshSettings.resolution + 1) + meshSettings.resolution,
+        (i + 1) * (meshSettings.resolution + 1) + meshSettings.resolution,
         0
       )
     );
   }
-}
 
-for (let i = 0; i < resolution; i++) {
-  myGeoMesh.connections.push(
-    new Vec3(
-      resolution * (resolution + 1) + i,
-      resolution * (resolution + 1) + i + 1,
-      0
-    ),
-    new Vec3(
-      i * (resolution + 1) + resolution,
-      (i + 1) * (resolution + 1) + resolution,
-      0
-    )
-  );
-}
-
-for (let y = 0; y < resolution; y++) {
-  for (let x = 0; x < resolution; x++) {
-    myGeoMesh.triangles.push(
-      new Vec3(
-        x + y * (resolution + 1),
-        1 + x + y * (resolution + 1),
-        resolution + 1 + x + y * (resolution + 1)
-      ),
-      new Vec3(
-        1 + x + y * (resolution + 1),
-        resolution + 1 + x + y * (resolution + 1),
-        resolution + 2 + x + y * (resolution + 1)
-      )
-    );
+  for (let y = 0; y < meshSettings.resolution; y++) {
+    for (let x = 0; x < meshSettings.resolution; x++) {
+      outputMesh.triangles.push(
+        new Vec3(
+          x + y * (meshSettings.resolution + 1),
+          1 + x + y * (meshSettings.resolution + 1),
+          meshSettings.resolution + 1 + x + y * (meshSettings.resolution + 1)
+        ),
+        new Vec3(
+          1 + x + y * (meshSettings.resolution + 1),
+          meshSettings.resolution + 1 + x + y * (meshSettings.resolution + 1),
+          meshSettings.resolution + 2 + x + y * (meshSettings.resolution + 1)
+        )
+      );
+    }
   }
+  return outputMesh;
 }
 
+let myGeoMesh = initialize()
 myGeoMesh = myGeoMesh.applyMetric(metric);
+
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -112,20 +145,36 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('display').appendChild(renderer.domElement);
 
-const growProbability = 0.01;
-const growFactor = 2;
+const annealSettings = {
+
+  'scale likelihood': 0.01,
+  'scale rate': 2,
+  'flatten rate': 0.01,
+  'stretch': ()=> myGeoMesh = myGeoMesh.stretch(2)
+}
 let stepSize = 1;
+const gui = new dat.GUI();
+const initFolder = gui.addFolder('Init Settings')
+const annealFolder = gui.addFolder('Anneal Controls')
+annealFolder.add(annealSettings, 'scale likelihood', 0, 1)
+annealFolder.add(annealSettings, 'scale rate', 1, 4)
+annealFolder.add(annealSettings, 'flatten rate', 0, 0.1)
+annealFolder.add(annealSettings, 'stretch')
+
+annealFolder.open();
+gui.open();
 function animate() {
-  console.log(stepSize)
+  console.log(myGeoMesh.loss());
   let newMesh = myGeoMesh.step(stepSize);
   if (newMesh.loss() < myGeoMesh.loss()) {
     myGeoMesh = newMesh;
-    if (Math.random() < growProbability) {
-      stepSize *= growFactor;
+    if (Math.random() < annealSettings['scale likelihood']) {
+      stepSize *= annealSettings['scale rate'];
     }
   } else {
-    stepSize /= growFactor;
+    stepSize /= annealSettings['scale rate'];
   }
+  myGeoMesh = myGeoMesh.flatten(annealSettings['flatten rate']);
   const wireframeGeometry = new THREE.WireframeGeometry(fromGeoMesh(myGeoMesh));
   const wireframeMaterial = new THREE.LineBasicMaterial({
     color: 0xffffff,
@@ -135,6 +184,7 @@ function animate() {
     wireframeGeometry,
     wireframeMaterial
   );
+  wireframeMesh.geometry.center();
   wireframeMesh.rotateX(Math.PI * 0.5);
   scene.add(wireframeMesh);
 
--- _src/manifolds/topologysolver.ts
+++ _src/manifolds/topologysolver.ts
@@ -4,14 +4,19 @@ export class GeoMesh {
   points: Vec3[];
   connections: Vec3[]; //a, b, desired length
   triangles: Vec3[];
+  flattenWeights: number[];
   constructor() {
-    (this.points = []), (this.connections = []), (this.triangles = []);
+    (this.points = []),
+      (this.connections = []),
+      (this.triangles = []),
+      (this.flattenWeights = []);
   }
   clone(): GeoMesh {
     let out = new GeoMesh();
     out.points = this.points.map(a => a.clone());
     out.connections = this.connections.map(a => a.clone());
     out.triangles = this.triangles.map(a => a.clone());
+    out.flattenWeights = this.flattenWeights.map(a => a);
     return out;
   }
   loss(): number {
@@ -47,19 +52,49 @@ export class GeoMesh {
     }
     return out;
   }
+  flatten(dt): GeoMesh {
+    let derivative: Vec3[] = [];
+    for (let _ = 0; _ < this.points.length; _++) {
+      derivative.push(new Vec3(0, 0, 0));
+    }
+    this.connections.forEach(connection => {
+      let a = this.points[connection.x];
+      let b = this.points[connection.y];
+      let difference = b.sub(a);
+      let connectionDerivative = difference.norm().mul(-1);
+      derivative[connection.x] =
+        derivative[connection.x].sub(connectionDerivative);
+      derivative[connection.y] =
+        derivative[connection.y].add(connectionDerivative);
+    });
+    let out = this.clone();
+    for (let i = 0; i < out.points.length; i++) {
+      out.points[i] = out.points[i].add(
+        derivative[i].mul(dt * out.flattenWeights[i])
+      );
+    }
+    return out;
+  }
+  stretch(factor): GeoMesh {
+    let out = this.clone();
+    out.points = out.points.map(point => point.mul(factor));
+    return out;
+  }
   applyMetric(metric: (Vec2) => [number, number, number, number]): GeoMesh {
-    let out = this.clone()
+    let out = this.clone();
     out.connections = out.connections.map(connection => {
-        let difference = out.points[connection.y].sub(out.points[connection.x]);
-        let average = out.points[connection.y].sub(out.points[connection.x]).mul(0.5);
-        let localMetric = metric(new Vec2(average.x, average.y));
-        let lengthSquared =
-            localMetric[0] * difference.x * difference.x + 
-            localMetric[1] * difference.x * difference.y + 
-            localMetric[2] * difference.y * difference.x + 
-            localMetric[3] * difference.y * difference.y
-        return new Vec3(connection.x, connection.y, Math.sqrt(lengthSquared))
-    })
-    return out
+      let difference = out.points[connection.y].sub(out.points[connection.x]);
+      let average = out.points[connection.y]
+        .add(out.points[connection.x])
+        .mul(0.5);
+      let localMetric = metric(new Vec2(average.x, average.y));
+      let lengthSquared =
+        localMetric[0] * difference.x * difference.x +
+        localMetric[1] * difference.x * difference.y +
+        localMetric[2] * difference.y * difference.x +
+        localMetric[3] * difference.y * difference.y;
+      return new Vec3(connection.x, connection.y, Math.sqrt(lengthSquared));
+    });
+    return out;
   }
 }
