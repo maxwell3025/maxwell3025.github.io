1eda8755bca87f24e3e0b1477a472bcb4ae4676f
maxwell3025 <44588341+maxwell3025@users.noreply.github.com>
2022-11-18 12:14:59 -0500

    Updated Build Process To Be More Modular
    -each page is now its own object in the target list
    now prints when it finishes building

--- build.js
+++ build.js
@@ -3,32 +3,85 @@ const postcss = require('postcss');
 const webpackConfig = require('./webpack.config');
 const postcssConfig = require('./postcss.config');
 const fs = require('node:fs');
+const path = require('node:path');
 
-htmls = ['button/index', 'gravity/index', 'handwriting/index', 'wall/index', 'home/index', 'manifolds/index']
+const pages = [
+  {
+    html: 'button/index',
+    source: 'button/button',
+    style: 'button/button',
+  },
+  {
+    html: 'gravity/index',
+    source: 'gravity/gravity',
+    style: 'gravity/gravity',
+  },
+  {
+    html: 'handwriting/index',
+    source: 'handwriting/main',
+    style: '',
+  },
+  {
+    html: 'wall/index',
+    source: 'wall/wall',
+    style: 'wall/wall',
+  },
+  {
+    html: 'home/index',
+    source: 'home/main',
+    style: 'home/home',
+  },
+  {
+    html: 'manifolds/index',
+    source: 'manifolds/manifolds',
+    style: '',
+  },
+];
 
-sources = ['home/main', 'handwriting/main', 'gravity/gravity', 'wall/wall', 'button/button', 'manifolds/manifolds'];
+pages.forEach(page => {
+  console.log(`started compiling ${page.html}!`);
+  if (page.html != '') {
+    fs.mkdirSync(path.dirname(page.html), { recursive: true });
+    let pageData = fs.readFileSync('_src/' + page.html + '.html');
+    fs.writeFileSync(page.html + '.html', pageData);
+  }
 
-styles = ['home/home', 'gravity/gravity', 'wall/wall', 'button/button'];
-
-htmls.forEach(element => {
-  fs.writeFileSync(element + '.html', fs.readFileSync('_src/' + element + '.html'))
-})
-
-sources.forEach(element => {
-  webpack(webpackConfig(element), (err, stats) => {
-    if (err) {
-      console.log(err);
-    }
-    if (stats.hasErrors()) {
-      console.log(new Error(stats.compilation.errors.join('\n')));
+  let sourcePromise = new Promise((resolve, reject) => {
+    if (page.source != '') {
+      fs.mkdirSync(path.dirname(page.source), { recursive: true });
+      webpack(webpackConfig(page.source), (err, stats) => {
+        if (err) {
+          console.log(err);
+          reject();
+        }
+        if (stats.hasErrors()) {
+          console.log(new Error(stats.compilation.errors.join('\n')));
+          reject();
+        }
+        resolve();
+      });
+    } else {
+      resolve();
     }
   });
-});
 
-styles.forEach(element => {
-  postcss(postcssConfig.plugins)
-    .process(fs.readFileSync('_src/' + element + '.css'))
-    .then(result => {
-      fs.writeFileSync(element + '.css', result.css);
+  let stylePromise = new Promise((resolve, reject) => {
+    if (page.style != '') {
+      fs.mkdirSync(path.dirname(page.style), { recursive: true });
+      postcss(postcssConfig.plugins)
+        .process(fs.readFileSync('_src/' + page.style + '.css'))
+        .then(result => {
+          fs.writeFileSync(page.style + '.css', result.css);
+          resolve();
+        });
+    } else {
+      resolve();
+    }
+  });
+
+  sourcePromise.then(() => {
+    stylePromise.then(() => {
+      console.log(`finished compiling ${page.html}!`);
     });
+  });
 });
