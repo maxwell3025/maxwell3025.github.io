91eaa62a856d8ae58bf7941bbdc5a3b2299da267
maxwell3025 <44588341+maxwell3025@users.noreply.github.com>
2022-11-26 22:53:45 -0500

    Clean Up

--- _src/quantumcomputer/CircuitOption.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { QuantumGate } from './quantum';
-
-export default function CircuitOption(properties: {
-  gate: QuantumGate;
-  appendGate: (gate: QuantumGate) => void;
-}) {
-  return <button onClick={event => {properties.appendGate(properties.gate)}}></button>;
-}
