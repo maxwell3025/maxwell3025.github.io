bd844d00b419a56edb9d28e9ad4a0da01c4e1b6a
maxwell3025 <44588341+maxwell3025@users.noreply.github.com>
2022-12-08 11:34:56 -0500

    Set Up Env For English Project
    -finished react setup
    -created and built test page

---
+++ _src/engwrit-112/Index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export default function Index(){
+    return <div className="bg-red-500 w-10 h-10"></div>
+}
--- build.js
+++ build.js
@@ -6,35 +6,40 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const pages = [
+  // {
+  //   html: 'button/index',
+  //   source: 'button/button',
+  //   style: 'button/button',
+  // },
+  // {
+  //   html: 'gravity/index',
+  //   source: 'gravity/gravity',
+  //   style: 'gravity/gravity',
+  // },
+  // {
+  //   html: 'handwriting/index',
+  //   source: 'handwriting/main',
+  //   style: '',
+  // },
+  // {
+  //   html: 'wall/index',
+  //   source: 'wall/wall',
+  //   style: 'wall/wall',
+  // },
+  // {
+  //   html: 'home/index',
+  //   source: 'home/main',
+  //   style: 'home/home',
+  // },
+  // {
+  //   html: 'manifolds/index',
+  //   source: 'manifolds/manifolds',
+  //   style: '',
+  // },
   {
-    html: 'button/index',
-    source: 'button/button',
-    style: 'button/button',
-  },
-  {
-    html: 'gravity/index',
-    source: 'gravity/gravity',
-    style: 'gravity/gravity',
-  },
-  {
-    html: 'handwriting/index',
-    source: 'handwriting/main',
-    style: '',
-  },
-  {
-    html: 'wall/index',
-    source: 'wall/wall',
-    style: 'wall/wall',
-  },
-  {
-    html: 'home/index',
-    source: 'home/main',
-    style: 'home/home',
-  },
-  {
-    html: 'manifolds/index',
-    source: 'manifolds/manifolds',
-    style: '',
+    html: 'engwrit-112/index',
+    source: 'engwrit-112/main',
+    style: 'engwrit-112/style'
   },
 ];
 
