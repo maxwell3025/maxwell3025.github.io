52f2e601528b83b4e7fe6155cc293046eef1ba33
Maxwell Tang <amoinus@gmail.com>
2023-09-10 14:01:43 -0400

    Migrated into new project

--- quantumcomputer/src/quantumcomputer.ts
+++ quantumcomputer/src/quantumcomputer.ts
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import CircuitEditor from "./CircuitEditor";
+import 'quantumcomputer.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(React.createElement(CircuitEditor, {initialWidth: 2, initialColumnCount: 4}))
