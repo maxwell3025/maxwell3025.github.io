324fdb9861c07a82673bc31ad765c6f70b98725b
maxwell3025 <amoinus@gmail.com>
2024-06-17 20:18:31 -0400

    Full saving/loading interface

--- electromagnetism/src/fieldSaving.js
+++ electromagnetism/src/fieldSaving.js
@@ -38,9 +38,41 @@ export async function saveZip(instance){
     const zipFileWriter = new BlobWriter("application/x-zip");
     const zipWriter = new ZipWriter(zipFileWriter);
 
-    await addField(zipWriter, instance.fieldConductivityX, "conductivity-x.png");
-    await addField(zipWriter, instance.fieldConductivityY, "conductivity-y.png");
-    await addField(zipWriter, instance.fieldConductivityZ, "conductivity-z.png");
+    await addField(zipWriter, instance.fieldDX.srcTexture,      "displacement-x-src.png");
+    await addField(zipWriter, instance.fieldDX.destTexture,     "displacement-x-dest.png");
+    await addField(zipWriter, instance.fieldDX.solnTexture,     "displacement-x-soln.png");
+    await addField(zipWriter, instance.fieldDY.srcTexture,      "displacement-y-src.png");
+    await addField(zipWriter, instance.fieldDY.destTexture,     "displacement-y-dest.png");
+    await addField(zipWriter, instance.fieldDY.solnTexture,     "displacement-y-soln.png");
+    await addField(zipWriter, instance.fieldDZ.srcTexture,      "displacement-z-src.png");
+    await addField(zipWriter, instance.fieldDZ.destTexture,     "displacement-z-dest.png");
+    await addField(zipWriter, instance.fieldDZ.solnTexture,     "displacement-z-soln.png");
+    await addField(zipWriter, instance.fieldBX.srcTexture,      "magnetism-x-src.png");
+    await addField(zipWriter, instance.fieldBX.destTexture,     "magnetism-x-dest.png");
+    await addField(zipWriter, instance.fieldBX.solnTexture,     "magnetism-x-soln.png");
+    await addField(zipWriter, instance.fieldBY.srcTexture,      "magnetism-y-src.png");
+    await addField(zipWriter, instance.fieldBY.destTexture,     "magnetism-y-dest.png");
+    await addField(zipWriter, instance.fieldBY.solnTexture,     "magnetism-y-soln.png");
+    await addField(zipWriter, instance.fieldBZ.srcTexture,      "magnetism-z-src.png");
+    await addField(zipWriter, instance.fieldBZ.destTexture,     "magnetism-z-dest.png");
+    await addField(zipWriter, instance.fieldBZ.solnTexture,     "magnetism-z-soln.png");
+    await addField(zipWriter, instance.fieldCharge.srcTexture,  "charge-src.png");
+    await addField(zipWriter, instance.fieldCharge.destTexture, "charge-dest.png");
+    await addField(zipWriter, instance.fieldCharge.solnTexture, "charge-soln.png");
+    await addField(zipWriter, instance.fieldJX,                 "current-x.png");
+    await addField(zipWriter, instance.fieldJY,                 "current-y.png");
+    await addField(zipWriter, instance.fieldJZ,                 "current-z.png");
+    await addField(zipWriter, instance.fieldFreq,               "frequency.png");
+    await addField(zipWriter, instance.fieldInvPermittivityX,   "inv-permittivity-x.png");
+    await addField(zipWriter, instance.fieldInvPermittivityY,   "inv-permittivity-y.png");
+    await addField(zipWriter, instance.fieldInvPermittivityZ,   "inv-permittivity-z.png");
+    await addField(zipWriter, instance.fieldInvPermeabilityX,   "inv-permeability-x.png");
+    await addField(zipWriter, instance.fieldInvPermeabilityY,   "inv-permeability-y.png");
+    await addField(zipWriter, instance.fieldInvPermeabilityZ,   "inv-permeability-z.png");
+    await addField(zipWriter, instance.fieldConductivityX,      "conductivity-x.png");
+    await addField(zipWriter, instance.fieldConductivityY,      "conductivity-y.png");
+    await addField(zipWriter, instance.fieldConductivityZ,      "conductivity-z.png");
+    await addField(zipWriter, instance.fieldMaterial,           "material.png");
 
     const metadata = {
         width: instance.width,
@@ -93,6 +125,99 @@ export function validateMetadata(object){
  */
 function writeField(name, instance, data){
     switch(name){
+        case "displacement-x-src.png":
+            instance.fieldDX.srcTexture.setData(data);
+            break;
+        case "displacement-x-dest.png":
+            instance.fieldDX.destTexture.setData(data);
+            break;
+        case "displacement-x-soln.png":
+            instance.fieldDX.solnTexture.setData(data);
+            break;
+        case "displacement-y-src.png":
+            instance.fieldDY.srcTexture.setData(data);
+            break;
+        case "displacement-y-dest.png":
+            instance.fieldDY.destTexture.setData(data);
+            break;
+        case "displacement-y-soln.png":
+            instance.fieldDY.solnTexture.setData(data);
+            break;
+        case "displacement-z-src.png":
+            instance.fieldDZ.srcTexture.setData(data);
+            break;
+        case "displacement-z-dest.png":
+            instance.fieldDZ.destTexture.setData(data);
+            break;
+        case "displacement-z-soln.png":
+            instance.fieldDZ.solnTexture.setData(data);
+            break;
+        case "magnetism-x-src.png":
+            instance.fieldBX.srcTexture.setData(data);
+            break;
+        case "magnetism-x-dest.png":
+            instance.fieldBX.destTexture.setData(data);
+            break;
+        case "magnetism-x-soln.png":
+            instance.fieldBX.solnTexture.setData(data);
+            break;
+        case "magnetism-y-src.png":
+            instance.fieldBX.srcTexture.setData(data);
+            break;
+        case "magnetism-y-dest.png":
+            instance.fieldBX.destTexture.setData(data);
+            break;
+        case "magnetism-y-soln.png":
+            instance.fieldBX.solnTexture.setData(data);
+            break;
+        case "magnetism-z-src.png":
+            instance.fieldBZ.srcTexture.setData(data);
+            break;
+        case "magnetism-z-dest.png":
+            instance.fieldBZ.destTexture.setData(data);
+            break;
+        case "magnetism-z-soln.png":
+            instance.fieldBZ.solnTexture.setData(data);
+            break;
+        case "charge-src.png":
+            instance.fieldCharge.srcTexture.setData(data);
+            break;
+        case "charge-dest.png":
+            instance.fieldCharge.destTexture.setData(data);
+            break;
+        case "charge-soln.png":
+            instance.fieldCharge.solnTexture.setData(data);
+            break;
+        case "current-x.png":
+            instance.fieldJX.setData(data);
+            break;
+        case "current-y.png":
+            instance.fieldJY.setData(data);
+            break;
+        case "current-z.png":
+            instance.fieldJZ.setData(data);
+            break;
+        case "frequency.png":
+            instance.fieldFreq.setData(data);
+            break;
+        case "inv-permittivity-x.png":
+            instance.fieldInvPermittivityX.setData(data);
+            break;
+        case "inv-permittivity-y.png":
+            instance.fieldInvPermittivityY.setData(data);
+            break;
+        case "inv-permittivity-z.png":
+            instance.fieldInvPermittivityZ.setData(data);
+            break;
+        case "inv-permeability-x.png":
+            instance.fieldInvPermeabilityX.setData(data);
+            break;
+        case "inv-permeability-y.png":
+            instance.fieldInvPermeabilityY.setData(data);
+            break;
+        case "inv-permeability-z.png":
+            instance.fieldInvPermeabilityZ.setData(data);
+            break;
         case "conductivity-x.png":
             instance.fieldConductivityX.setData(data);
             break;
@@ -101,6 +226,9 @@ function writeField(name, instance, data){
             break;
         case "conductivity-z.png":
             instance.fieldConductivityZ.setData(data);
+            break;
+        case "material.png":
+            instance.fieldMaterial.setData(data);
             break;
         default:
             throw new Error(`No destination for ${name}`);
